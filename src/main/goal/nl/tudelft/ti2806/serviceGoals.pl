57f1ffe2f589e9b062c8f9d2481a77ae7d8ff257
ardwennem2 <tom_brunner@hotmail.com>
2016-05-26 16:45:48 +0200

    added indicator and stakeholder as beliefs and implemented (in comments) the new goal for terrace

--- src/main/goal/nl/tudelft/ti2806/serviceGoals.pl
+++ src/main/goal/nl/tudelft/ti2806/serviceGoals.pl
@@ -1,12 +1,12 @@
 %goals from indicators:
 haveTerrace.
 buildStore.
-buildSportsCenter
+buildSportsCenter.
 %translators needed: stakeholder for budget, buildings for current amount of buildings, indicators for the indicator, 
 %land for the land to build, category for the type of land, j2zone for zone rules, and popupdata for seeing when your buildings are accepted by government
-buyLand. 
+% buyLand. 
 %this goal will be made by buildbuilding if you don't have enough land to build the buildings: you need translators: 
 %stakeholder for budget, land for land and popupdata for seeing when you have bought the land.
-demolishBuilding. 
+% demolishBuilding. 
 %This goal will be made after buyLand has been completed if there are still buildings on the land, you need translators: 
 %stakeholder for budget, land for land, buildings for buildings on the land.
--- src/main/goal/nl/tudelft/ti2806/serviceKnowledge.pl
+++ src/main/goal/nl/tudelft/ti2806/serviceKnowledge.pl
@@ -3,6 +3,8 @@
 :- dynamic settings/1.
 :- dynamic functions/1.
 :- dynamic buildings/1.
+:- dynamic indicator/1.
+:- dynamic stakeholder/1.
 
 % knowledge:
 :- dynamic demolished/1.
@@ -10,6 +12,7 @@
 
 demolishBuilding(ID) :- demolished(ID).
 haveTerrace :- constructed(Id, Name), (Name == terrace; Name = terras).
+%haveTerrace :- stakeholder(ID, Name, Budget, Income, Link(ID2, Indicator(ID3, IndicatorName, weight), indicator(ID4, value, target), Name == 'Voorzieningen', IndicatorName='Terras Indicator (2)', ID4==ID3, value>=target.
 buildStore :- constructed(ID, Name), (Name =='buurtwinkel'; Name == 'convenience store').
 buildSportsCenter :- constructed(ID, Name), (Name =='sportcentrum'; Name == 'sports center').
-buyLand :- 
+%buyLand :- 
