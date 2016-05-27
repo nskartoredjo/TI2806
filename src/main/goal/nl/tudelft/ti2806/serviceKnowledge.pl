c125a686d2c49cef2226d99c4d61002d399cbe9f
ardwennem2 <tom_brunner@hotmail.com>
2016-05-27 15:45:04 +0200

    the gaols works
    thanx to Jannelie

--- src/main/goal/nl/tudelft/ti2806/serviceKnowledge.pl
+++ src/main/goal/nl/tudelft/ti2806/serviceKnowledge.pl
@@ -3,16 +3,19 @@
 :- dynamic settings/1.
 :- dynamic functions/1.
 :- dynamic buildings/1.
-:- dynamic indicator/1.
-:- dynamic stakeholder/1.
+:- dynamic indicators/1.
+:- dynamic stakeholders/1.
 
 % knowledge:
 :- dynamic demolished/1.
 :- dynamic constructed/2.
 
 demolishBuilding(ID) :- demolished(ID).
-haveTerrace :- constructed(Id, Name), (Name == terrace; Name = terras).
-%haveTerrace :- stakeholder(ID, Name, Budget, Income, Link(ID2, Indicator(ID3, IndicatorName, weight), indicator(ID4, value, target), Name == 'Voorzieningen', IndicatorName='Terras Indicator (2)', ID4==ID3, value>=target.
-buildStore :- constructed(ID, Name), (Name =='buurtwinkel'; Name == 'convenience store').
-buildSportsCenter :- constructed(ID, Name), (Name =='sportcentrum'; Name == 'sports center').
+%haveTerrace :- constructed(Id, Name), (Name == terrace; Name = terras). OLD
+
+
+%These should work however tygrons indicators still don't work so we can not test these. appart from there may be an error if multiple stakeholders make use of the same indicator since this changes the indicator names.
+buildStore :- stakeholders(L), member((stakeholder(ID, Name, Budget, Income), indictorLink(ID, IndicatorL)), L), member(indicatorWeights(IndID, IndName,Weight), IndicatorL), IndName == 'Buurtwinkel Indicator', Name == 'Voorzieningen', indicators(Indicators), member(indicator(IndID, Value, Target),Indicators), Value >= Target.
+buildSportsCenter :- stakeholders(L), member((stakeholder(ID, Name, Budget, Income), indictorLink(ID, IndicatorL)), L), member(indicatorWeights(IndID, IndName,Weight), IndicatorL), IndName == 'Sportcentrum Indicator', Name == 'Voorzieningen', indicators(Indicators), member(indicator(IndID, Value, Target),Indicators), Value >= Target.
+haveTerrace :- stakeholders(L), member((stakeholder(ID, Name, Budget, Income), indictorLink(ID, IndicatorL)), L), member(indicatorWeights(IndID, IndName,Weight), IndicatorL), IndName == 'Terras Indicator', Name == 'Voorzieningen', indicators(Indicators), member(indicator(IndID, Value, Target),Indicators), Value >= Target.
 %buyLand :- 
