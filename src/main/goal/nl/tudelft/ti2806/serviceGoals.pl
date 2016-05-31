24e3f0c44747e55f243f00c8da325197e503a9d1
N.S. Kartoredjo <N.S.Kartoredjo@student.tudelft.nl>
2016-05-31 10:32:36 +0200

    changed comments
    - removed commented code
    - add notes and moved comments to node
    - add TODOs
    - add descriptions

--- src/main/goal/nl/tudelft/ti2806/serviceGoals.pl
+++ src/main/goal/nl/tudelft/ti2806/serviceGoals.pl
@@ -1,12 +1,14 @@
-%goals from indicators:
+%%% goals from indicators %%%
 haveTerrace.
 buildStore.
 buildSportsCenter.
-%translators needed: stakeholder for budget, buildings for current amount of buildings, indicators for the indicator, 
-%land for the land to build, category for the type of land, j2zone for zone rules, and popupdata for seeing when your buildings are accepted by government
-% buyLand. 
-%this goal will be made by buildbuilding if you don't have enough land to build the buildings: you need translators: 
-%stakeholder for budget, land for land and popupdata for seeing when you have bought the land.
-% demolishBuilding. 
-%This goal will be made after buyLand has been completed if there are still buildings on the land, you need translators: 
-%stakeholder for budget, land for land, buildings for buildings on the land.
+
+%%% NOTES %%%
+% - translators needed: stakeholder for budget, buildings for current amount of buildings, indicators for the indicator, 
+% land for the land to build, category for the type of land, j2zone for zone rules, and popupdata for seeing when your buildings are accepted by government
+
+% - buyLand: this goal will be made by buildbuilding if you don't have enough land to build the buildings: you need translators: 
+% stakeholder for budget, land for land and popupdata for seeing when you have bought the land.
+
+% - demolishBuilding: This goal will be made after buyLand has been completed if there are still buildings on the land, you need translators: 
+% stakeholder for budget, land for land, buildings for buildings on the land.
--- src/main/goal/nl/tudelft/ti2806/serviceKnowledge.pl
+++ src/main/goal/nl/tudelft/ti2806/serviceKnowledge.pl
@@ -1,4 +1,4 @@
-% percepts:
+%%% percepts %%%
 :- dynamic stakeholders/1.
 :- dynamic settings/1.
 :- dynamic functions/1.
@@ -6,16 +6,52 @@
 :- dynamic indicators/1.
 :- dynamic stakeholders/1.
 
-% knowledge:
+%%% knowledge %%%
 :- dynamic demolished/1.
 :- dynamic constructed/2.
 
-demolishBuilding(ID) :- demolished(ID).
-%haveTerrace :- constructed(Id, Name), (Name == terrace; Name = terras). OLD
+% description:
+% - TODO
+demolishBuilding(ID) :- 
+	demolished(ID).
 
+% description: 
+% - TODO
+buildStore :- 
+	stakeholders(L), 
+	member((stakeholder(ID, Name, Budget, Income), 
+	indictorLink(ID, IndicatorL)), L), 
+	member(indicatorWeights(IndID, IndName,Weight), IndicatorL), 
+	IndName == 'Buurtwinkel Indicator', 
+	Name == 'Voorzieningen', 
+	indicators(Indicators), 
+	member(indicator(IndID, Value, Target),Indicators), 
+	Value >= Target.
 
-%These should work however tygrons indicators still don't work so we can not test these. appart from there may be an error if multiple stakeholders make use of the same indicator since this changes the indicator names.
-buildStore :- stakeholders(L), member((stakeholder(ID, Name, Budget, Income), indictorLink(ID, IndicatorL)), L), member(indicatorWeights(IndID, IndName,Weight), IndicatorL), IndName == 'Buurtwinkel Indicator', Name == 'Voorzieningen', indicators(Indicators), member(indicator(IndID, Value, Target),Indicators), Value >= Target.
-buildSportsCenter :- stakeholders(L), member((stakeholder(ID, Name, Budget, Income), indictorLink(ID, IndicatorL)), L), member(indicatorWeights(IndID, IndName,Weight), IndicatorL), IndName == 'Sportcentrum Indicator', Name == 'Voorzieningen', indicators(Indicators), member(indicator(IndID, Value, Target),Indicators), Value >= Target.
-haveTerrace :- stakeholders(L), member((stakeholder(ID, Name, Budget, Income), indictorLink(ID, IndicatorL)), L), member(indicatorWeights(IndID, IndName,Weight), IndicatorL), IndName == 'Terras Indicator', Name == 'Voorzieningen', indicators(Indicators), member(indicator(IndID, Value, Target),Indicators), Value >= Target.
-%buyLand :- 
+% description:
+% - TODO
+buildSportsCenter :- 
+	stakeholders(L), 
+	member((stakeholder(ID, Name, Budget, Income), 
+	indictorLink(ID, IndicatorL)), L), 
+	member(indicatorWeights(IndID, IndName,Weight), IndicatorL), 
+	IndName == 'Sportcentrum Indicator', 
+	Name == 'Voorzieningen', indicators(Indicators), 
+	member(indicator(IndID, Value, Target),Indicators), 
+	Value >= Target.
+
+% description:
+% - TODO	
+buildTerrace :- 
+	stakeholders(L), 
+	member((stakeholder(ID, Name, Budget, Income), 
+	indictorLink(ID, IndicatorL)), L), 
+	member(indicatorWeights(IndID, IndName,Weight), IndicatorL), 
+	IndName == 'Terras Indicator', 
+	Name == 'Voorzieningen', 
+	indicators(Indicators), 
+	member(indicator(IndID, Value, Target),Indicators), 
+	Value >= Target.
+
+%%% NOTES %%% 
+% - Tygrons indicators still don't work so we can not test these. appart from there may be an error if multiple stakeholders make use of the same indicator since this changes the indicator names.
